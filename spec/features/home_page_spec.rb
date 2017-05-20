0cbe3226a771cd5373f5c11825754a0c72f5d0af
Kathleen Lan <kathleenlan@users.noreply.github.com>
2017-05-20 17:25:06 -0400

    Fix broken spec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -3,10 +3,6 @@ describe 'home page', :type => :feature do
     visit '/'
   end
 
-  it 'has a button for adding a pronounceable' do
-    expect(page).to have_content('Add a pronounceable')
-  end
-
   it 'has a search bar' do
     expect(find('input[type="search"]')).to be_truthy
   end
